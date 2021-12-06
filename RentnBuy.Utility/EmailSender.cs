212862887954bb7edf160f4daf3438bfc465ae4f
taha1998-Gitshit <61309914+taha1998-Gitshit@users.noreply.github.com>
2021-12-06 20:28:30 +0100

    fixed mail sending.

--- RentnBuy.Utility/EmailSender.cs
+++ RentnBuy.Utility/EmailSender.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity.UI.Services;
 using System;
 using System.Collections.Generic;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +10,36 @@ namespace RentnBuy.Utility
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("projet.net.stm@gmail.com");
+            mailMessage.To.Add(new MailAddress(email));
+
+            mailMessage.Subject = subject;
+            mailMessage.IsBodyHtml = true;
+
+            mailMessage.Body = "<h2> Dear Customer You registered an account on Rent&Buy Website, before being able to use" +
+            " your account you need to verify that this is your email address by clicking here </h2><br>" +
+            "https://localhost:5001/Customer/Home" +
+            "<br><h2>Kind Regards, Rent&Buy</h2>";
+
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential("projet.net.stm@gmail.com", "X**123456");
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            client.Host = "smtp.gmail.com";
+            client.Port = 587;
+
+            try
+            {
+                client.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
