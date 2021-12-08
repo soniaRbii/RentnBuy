87a0b84eff384c3b2691ff36d567e1c09a9ecb75
MeriamHamdani <mariem.hamdani@enis.tn>
2021-12-08 15:26:48 +0100

    fix login

--- RentnBuy.Utility/SD.cs
+++ RentnBuy.Utility/SD.cs
@@ -7,7 +7,7 @@ namespace RentnBuy.Utility
     public static class SD
     {
         public const string ManagerRole = "Manager";
-        public const string FrontDeskRole = "Front";
+        
         public const string CustomerRole = "Customer";
 
 
